e2cf938775968d8cce0398d78d5b8af507208008
Jamie Mason <jamie@foldleft.io>
2018-05-13 20:01:43 +0100

    feat(core): only return commits from the given directory

--- src/lib/read-git-log.ts
+++ src/lib/read-git-log.ts
@@ -7,7 +7,7 @@ const getNodes = (fields: IField[]) => fields.map(toNode).join('');
 export const readGitLog = (directory: string, fields: IField[], oldestFirst: boolean = false) => {
   const baseArgs = ['log', `--pretty=format:<commit>${getNodes(fields)}</commit>`, '--author-date-order'];
   const args = oldestFirst === true ? baseArgs.concat('--reverse') : baseArgs;
-  const task = spawn('git', args, { cwd: directory });
+  const task = spawn('git', [...args, '--', directory], { cwd: directory });
   task.stdout.setEncoding('utf8');
   task.stderr.setEncoding('utf8');
   return task;
